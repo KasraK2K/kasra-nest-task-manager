e79a00b7b355249c61ce34bc3c69f0b9949cd138
Kasra <Kasra.Karami.KK@gmail.com>
2020-06-09 16:36:15 -0500

    add jwt and passport

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -8,12 +8,10 @@ import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-
     const user = new User();
     user.username = username;
     user.salt = await bcrypt.genSalt();
     user.password = await this.hashPassword(password, user.salt);
-
     try {
       await user.save();
     } catch (e) {
@@ -22,6 +20,16 @@ export class UserRepository extends Repository<User> {
       } else {
         throw new InternalServerErrorException();
       }
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+    if (user && (await user.validatePassword(password))) {
+      return user.username;
+    } else {
+      return null;
     }
   }
 
